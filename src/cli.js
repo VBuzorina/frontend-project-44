2632fe146e010ca76e43f8bac8da66d5107eb805
VBuzorina <vitalina.buzorina@gmail.com>
2024-01-20 22:15:43 +0400

    main question logic

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,25 @@
 import readlineSync from 'readline-sync';
 
-export const greeting = () => {
+const greeting = () => {
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+};
+
+const question = () => {
+  const randomNum = Math.round(Math.random() * 100);
+  const isNumEven = randomNum % 2 === 0;
+  console.log('Question:', randomNum);
+  const answer = readlineSync.question('Your answer: ').toLowerCase() === 'yes';
+  if (isNumEven === answer) {
+    console.log('Correct!');
+  } else {
+    console.log('Incorrect!');
+  }
+};
+
+export const main = () => {
+  greeting();
+  question();
 };
