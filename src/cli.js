2968f8db0e1eb2049e253bd187faa3e3dbe8e467
VBuzorina <vitalina.buzorina@gmail.com>
2024-01-20 23:16:28 +0400

    complite logic

--- src/cli.js
+++ src/cli.js
@@ -5,21 +5,46 @@ const greeting = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  return name;
 };
 
-const question = () => {
+const question = (name, countGame) => {
   const randomNum = Math.round(Math.random() * 100);
   const isNumEven = randomNum % 2 === 0;
+
   console.log('Question:', randomNum);
-  const answer = readlineSync.question('Your answer: ').toLowerCase() === 'yes';
-  if (isNumEven === answer) {
+
+  const answer = readlineSync.question('Your answer: ');
+  const answerBool = answer.toLocaleLowerCase() === 'yes';
+
+  if (isNumEven === answerBool) {
     console.log('Correct!');
-  } else {
-    console.log('Incorrect!');
+
+    if (countGame > 1) {
+      return question(name, countGame - 1);
+    }
+
+    return true;
   }
+
+  if (answerBool) {
+    console.log(`"${answer}" is wrong answer ;(. Correct answer was "no".`);
+  } else {
+    console.log(`"${answer}" is wrong answer ;(. Correct answer was "yes".`);
+  }
+
+  return false;
 };
 
 export const main = () => {
-  greeting();
-  question();
+  const name = greeting();
+
+  const success = question(name, 3);
+
+  if (success) {
+    console.log(`Congratulations, ${name}!`);
+  } else {
+    console.log(`Let's try again, ${name}!`);
+  }
 };
